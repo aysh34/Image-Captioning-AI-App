b279db26527a705d258821a07ec2bf20ed17f6b9
Ayesha Saleem <134167523+aysh34@users.noreply.github.com>
2025-02-08 22:23:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-!pip install transformers requests torch
-
-# main app
 import gradio as gr
 import numpy as np
 from PIL import Image
@@ -18,19 +15,20 @@ def caption_image(input_image: np.ndarray):
     inputs = processor(raw_image, return_tensors="pt")
 
     # Generate a caption for the image
-    out = model.generate(**inputs,max_length=50)
+    out = model.generate(**inputs, max_length=50)
 
     # Decode the generated tokens to text
     caption = processor.decode(out[0], skip_special_tokens=True)
 
     return caption
 
+# Gradio interface
 iface = gr.Interface(
     fn=caption_image,
     inputs=gr.Image(),
     outputs="text",
     title="Image Captioning",
-    description="This is a simple web app for generating captions for images using a trained model."
+    description="Upload an image and get an AI-generated caption."
 )
 
 iface.launch()
